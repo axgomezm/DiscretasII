6a83e82089211d5f5ec5bd998893dddbb3e0946b
axgomezm <49238382+axgomezm@users.noreply.github.com>
2023-02-17 00:05:45 -0500

    Update TareaIIGrupos.py

--- TareaIIGrupos.py
+++ TareaIIGrupos.py
@@ -3,41 +3,41 @@ def cuadrado_y_grupo(matrix):
     grupo(matrix)
 def grupo(matrix):
 # verifica si se cumple el cierre
-    for i in range(len(matriz)):
-        for j in range(len(matriz)):
-            producto = matriz[i][j]
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            producto = matrix[i][j]
             tiene_producto = False
             for k in range(len(matriz)):
-                if matriz[j][k] * matriz[k][i] == producto:
+                if matrix[j][k] * matrix[k][i] == producto:
                     tiene_producto = True
                     break
             if not tiene_producto:
                 return False
     
     # verificar si es asociativa
-    for i in range(len(matriz)):
-        for j in range(len(matriz)):
-            for k in range(len(matriz)):
-                producto1 = matriz[i][j] * matriz[j][k] * matriz[k][i]
-                producto2 = matriz[i][k] * matriz[k][j] * matriz[j][i]
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            for k in range(len(matrix)):
+                producto1 = matrix[i][j] * matrix[j][k] * matrix[k][i]
+                producto2 = matrix[i][k] * matrix[k][j] * matrix[j][i]
                 if producto1 != producto2:
                     return False
     
     # verifica si existe la identidad
     identidad = None
-    for i in range(len(matriz)):
-        tiene_identidad = all(matriz[j][i] == matriz[i][j] for j in range(len(matriz)))
-        if tiene_identidad and matriz[i][i] == 1:
+    for i in range(len(matrix)):
+        tiene_identidad = all(matrix[j][i] == matrix[i][j] for j in range(len(matrix)))
+        if tiene_identidad and matrix[i][i] == 1:
             identidad = i
             break
     if identidad is None:
         return False
     
     # verifica si tiene inverso
-    for i in range(len(matriz)):
+    for i in range(len(matrix)):
         tiene_inverso = False
-        for j in range(len(matriz)):
-            if matriz[i][j] * matriz[j][i] == 1 and matriz[j][j] == 1:
+        for j in range(len(matrix)):
+            if matrix[i][j] * matrix[j][i] == 1 and matrix[j][j] == 1:
                 tiene_inverso = True
                 break
         if not tiene_inverso:
